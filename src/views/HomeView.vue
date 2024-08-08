663f4c34c195155c189720796c6e7b143ac3bb00
IndraHardika <92752408+KZdra@users.noreply.github.com>
2024-08-08 19:16:38 +0700

    first Upload

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+<h2 class="text-2xl text-purple-800 font-bold "> TEst Home</h2> 
+ </main>
+</template>
